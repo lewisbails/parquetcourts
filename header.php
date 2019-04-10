489a1f51d97eec2dddc0ed8dadc339be007bf2cb
lewisbails <lbai@oticon.com>
2019-04-10 18:03:13 +0200

    style-footer

--- header.php
+++ header.php
@@ -6,3 +6,4 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	    <!-- Bootstrap -->
   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  		<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/style-footer.css" type="text/css" rel="stylesheet" />
